edb2cc7145012de6b8c7af739df5ec0920e32a3c
BMaynardDev <brandonMaynard545@gmail.com>
2019-12-25 05:28:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udemyBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
